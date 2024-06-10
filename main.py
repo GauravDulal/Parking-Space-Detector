8d568f9813f37a3997d1ef0a40f041119ab95adf
Gaurav <garuav.dulal.50@gmail.com>
2024-06-10 16:08:33 +0545

    working on wen integration

--- main.py
+++ main.py
@@ -17,23 +17,49 @@ def checkParkingSpace(imgPro):
 
     for i, pos in enumerate(posList):
         x, y = pos
+
         imgCrop = imgPro[y : y + height, x : x + width]
         count = cv2.countNonZero(imgCrop)
 
         if count < 900:
+            color = (0, 255, 0)
+            thickness = 5
+            spaceCounter += 1
             status[f"space_{i+1}"] = "vacant"
         else:
+            color = (0, 0, 255)
+            thickness = 2
             status[f"space_{i+1}"] = "occupied"
 
-    return status
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), color, thickness)
+        cvzone.putTextRect(
+            img,
+            str(count),
+            (x, y + height - 3),
+            scale=1,
+            thickness=2,
+            offset=0,
+            colorR=color,
+        )
+
+    cvzone.putTextRect(
+        img,
+        f"Free: {spaceCounter}/{len(posList)}",
+        (100, 50),
+        scale=3,
+        thickness=5,
+        offset=20,
+        colorR=(0, 200, 0),
+    )
+
+    with open('parking_status.pkl', 'wb') as file:
+        pickle.dump(status, file)
 
 while True:
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
     success, img = cap.read()
-    if not success:
-        break
 
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
@@ -44,14 +70,12 @@ while True:
     kernel = np.ones((3, 3), np.uint8)
     imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
 
-    status = checkParkingSpace(imgDilate)
+    checkParkingSpace(imgDilate)
 
-    with open('parking_status.pkl', 'wb') as file:
-        pickle.dump(status, file)
-
-    # Optional: Display the frame for debugging
     cv2.imshow("Image", img)
-    if cv2.waitKey(10) & 0xFF == ord("q"):
+
+    key = cv2.waitKey(10)
+    if key == ord("q"):
         break
 
 cap.release()
